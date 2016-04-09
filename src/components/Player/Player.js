cd4de4130459f21924d35e7a084b6a0a96e4daa1
Goutham Veeramachaneni <cs14btech11014@iith.ac.in>
2016-04-09 20:52:58 +0530

    v0.1 of the audio player
    Signed-off-by: Goutham Veeramachaneni <cs14btech11014@iith.ac.in>

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -21,25 +21,51 @@ class Player extends React.Component {
   }
 
   // Helper Functions.
-  playa() {
+  play() {
     this.audio.play();
     this.props.playCurrent();
   }
+  pause() {
+    this.audio.pause();
+    this.props.pauseCurrent();
+  }
 
   componentDidMount() {
-    this.audio = new Audio(this.props.audio.current);
+    this.audio = new Audio(this.props.audio.current.url);
+    this.audio.volume = this.props.audio.volume;
+    this.audio.addEventListener('timeupdate', (target) => {
+      this.props.updateTime((target.currentTarget.currentTime * 100) / target.currentTarget.duration)
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.audio.current.url != nextProps.audio.current.url) {
+      this.audio.src = nextProps.audio.current.url;
+    }
+
+    nextProps.audio.playing ? this.audio.play() : this.audio.pause()
+    this.audio.volume = this.props.audio.volume;
+
+    if (Math.abs(this.props.audio.current.percentDone - nextProps.audio.current.percentDone) > 3) {
+      this.audio.currentTime = (nextProps.audio.current.percentDone * this.audio.duration) / 100;
+    }
   }
 
   render() {
+    let playButton = <button onClick={this.play.bind(this)}><i className={`fa fa-play-circle-o fa-2x ${styles.control}`}></i></button>
+    if (this.props.audio.playing) {
+      playButton = <button onClick={this.pause.bind(this)}><i className={`fa fa-pause-circle-o fa-2x ${styles.control}`}></i></button>
+    }
+
     return <div className={`container-fluid ${styles.player}`}>
       <div className={`row vertical-align`}>
         <div className={`${styles.controls} col-xs-3`}>
-          <button><i className={`fa fa-step-backward fa-2x ${styles.control}`}></i></button>
-          <button onClick={this.playa.bind(this)}><i className={`fa fa-play-circle-o fa-2x ${styles.control}`}></i></button>
-          <button><i className={`fa fa-step-forward fa-2x ${styles.control}`}></i></button>
+          <button onClick={this.props.playPrevious}><i className={`fa fa-step-backward fa-2x ${styles.control}`}></i></button>
+          {playButton}
+          <button onClick={this.props.playNext}><i className={`fa fa-step-forward fa-2x ${styles.control}`}></i></button>
         </div>
         <div className={`col-xs-2`}>
-          <RCslider onChange={log} />
+          <RCslider defaultValue={this.props.audio.volume * 100} onChange={this.props.changeVolume} tipFormatter={null} />
         </div>
         <div className='col-xs-6'>
           <div className='container-fluid'>
@@ -55,7 +81,7 @@ class Player extends React.Component {
                   <div>
                     <p>Coldplay - A Head full of Dreams</p>
                   </div>
-                  <RCslider onChange={log} />
+                  <RCslider value={this.props.audio.current.percentDone || 0} onChange={this.props.updateTime} tipFormatter={null} />
                 </div>
               </div>
             </div>
--- src/redux/modules/audio.js
+++ src/redux/modules/audio.js
@@ -1,12 +1,14 @@
 // Constants
 export const SET_CURRENT = 'SET_CURRENT'
 export const PLAY_CURRENT = 'PLAY_CURRENT'
-export const STOP_CURRENT = 'STOP_CURRENT'
+export const PAUSE_CURRENT = 'PAUSE_CURRENT'
 
 export const PLAY_NEXT = 'PLAY_NEXT'
 export const PLAY_PREV = 'PLAY_PREV'
 
 export const CHANGE_VOL = 'CHANGE_VOL'
+
+export const UPDATE_TIME = 'UPDATE_TIME'
 
 // action makers
 
@@ -23,19 +25,82 @@ export function playCurrent() {
   }
 }
 
-export const actions = {
-  setCurrent,
-  playCurrent
+export function pauseCurrent() {
+  return {
+    type: PAUSE_CURRENT
+  }
 }
 
-export default function audioReducer(state = {current: 'https://s3-ap-southeast-1.amazonaws.com/nvision/h.mp3'}, action) {
+export function playNext() {
+  return {
+    type: PLAY_NEXT
+  }
+}
+
+export function playPrevious() {
+  return {
+    type: PLAY_PREV
+  }
+}
+
+export function changeVolume(vol) {
+  vol = vol/100
+  return {
+    type: CHANGE_VOL,
+    value: vol
+  }
+}
+
+export function updateTime(pc) {
+  return {
+    type: UPDATE_TIME,
+    value: pc
+  }
+}
+
+export const actions = {
+  setCurrent,
+  playCurrent,
+  pauseCurrent,
+  playNext,
+  playPrevious,
+  changeVolume,
+  updateTime
+}
+
+export default function audioReducer(state = {current: {url: 'https://s3-ap-southeast-1.amazonaws.com/nvision/h.mp3', idx: 0, percentDone: 0}, playing: false, playlist: ['https://s3-ap-southeast-1.amazonaws.com/nvision/h.mp3', 'https://s3-ap-southeast-1.amazonaws.com/nvision/i.mp3'], volume: 0.5, changeTime: false}, action) {
   switch (action.type) {
     case SET_CURRENT:
-      return Object.assign({}, state, {current: action.url});
+      return Object.assign({}, state, {current: {url: action.url}});
       break;
     case PLAY_CURRENT:
-      return Object.assign({}, state, {playing: true});
+      let currentURL = state.current.url;
+      if (!currentURL) {
+        currentURL = state.playlist[0];
+      }
+      return Object.assign({}, state, {playing: true}, {current: {url: currentURL, idx: state.current.idx}});
       break;
+    case PAUSE_CURRENT:
+      return Object.assign({}, state, {playing: false});
+      break;
+    case PLAY_NEXT:
+      if (state.playlist.length > state.current.idx + 1) {
+        return Object.assign({}, state, {playing: true}, {current: {url: state.playlist[state.current.idx+1], idx: state.current.idx + 1}});
+      }
+      return Object.assign({}, state, {playing: false}, {current: {url: '', idx: 0}});
+      break;
+    case PLAY_PREV:
+      if (state.current.idx - 1 >= 0) {
+        return Object.assign({}, state, {playing: true}, {current: {url: state.playlist[state.current.idx-1], idx: state.current.idx - 1}});
+      }
+      return Object.assign({}, state, {playing: false}, {current: {url: '', idx: 0}});
+      break;
+    case CHANGE_VOL:
+      return Object.assign({}, state, {volume: action.value});
+    case UPDATE_TIME:
+      let current = Object.assign({}, state.current);
+      current.percentDone = action.value;
+      return Object.assign({}, state, {current: current});
     default:
       return state;
   }
